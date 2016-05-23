d56d5a1f8d22edc20d1c1d074073ecc598181776
apple <sj@sj.local>
2016-05-23 22:28:04 +0800

    Initial Commit

---
+++ LoadXIBWhenRunningStage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoadXIBWhenRunningStage
+//
+//  Created by Sj on 16/5/23.
+//  Copyright © 2016年 SJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
